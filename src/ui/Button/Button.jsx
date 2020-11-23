5deae3ea45ad764beef0917c4792d522ff64ec7b
Artyom <bitcoinfeks@gmail.com>
2020-11-23 17:20:49 +0500

    Добавил стилизованый Button

---
+++ src/ui/Button/Button.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  display: inline-block;
+
+  background-color: #9999FF;
+  padding: 10px 15px;
+
+  color: #fff;
+  font-size: 16px;
+`;
+
+export default Button;
