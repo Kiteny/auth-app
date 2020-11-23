d1b340047b4fe6d16e46becb483132b7e62d5c38
Artyom <bitcoinfeks@gmail.com>
2020-11-23 17:48:07 +0500

    Добавил роутинг

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+
+import { AuthForm, RegForm } from '../features/user';
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <Router>
+      <Switch>
+        <Route path="/auth" component={AuthForm} exact/>
+        <Route path="/reg" component={RegForm} exact/>
+        <Redirect to="/auth"/>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
