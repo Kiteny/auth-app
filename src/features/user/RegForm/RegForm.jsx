fe0eabc6c02130505f3f13c53018921574b68e44
Artyom <bitcoinfeks@gmail.com>
2020-11-23 17:23:50 +0500

    Добавил форму регистрации

---
+++ src/features/user/RegForm/RegForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form, { FormInput } from '../Form';
+
+const RegForm = () => {
+  return (
+    <Form header="Регистрация" buttonTitle="Регистрация">
+      <FormInput placeholder="Имя" />
+      <FormInput placeholder="Фамилия" />
+      <FormInput placeholder="Логин" />
+      <FormInput placeholder="Пароль" type="password" />
+      <FormInput placeholder="Повторить пароль"  type="password" />
+    </Form>
+  );
+}
+
+export default RegForm;
