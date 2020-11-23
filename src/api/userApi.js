fc36a0e9abdf37f77f15e45b1c5e609fd82b1bac
Artyom <bitcoinfeks@gmail.com>
2020-11-23 21:40:06 +0500

    Переименовал метод "register" в "signUp" в userApi

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -4,7 +4,7 @@ import { BASE_URL, COUNTRY_KEY, INVITED_BY } from './constansts';
 axios.defaults.baseURL = BASE_URL;
 
 export default {
-  register(email, password, phone, name, surname) {
+  signUp(email, password, phone, name, surname) {
     return axios('/clients/create/', {
       method: 'POST',
       data: {
@@ -19,5 +19,6 @@ export default {
         },
       }
     });
-  }
+  },
+
 }
