7ff74b3ecebf8833e97d9027871b5608c971f8fd
Artyom <bitcoinfeks@gmail.com>
2020-11-25 21:53:24 +0500

    Исправил редирект на форму авторизации после перезагрузки страницы на форме регистрации

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -12,27 +12,25 @@ const App = () => {
 
   if (!isSignIn ) {
     routes = (
-      <>
+      <Switch>
         <Route path="/login" component={AuthForm} exact/>
         <Route path="/signup" component={RegForm} exact/>
-        <Redirect to="/login"/>
-      </>
+        <Redirect from="/" to="/login"/>
+      </Switch>
     );
   } else {
     routes = (
-      <>
+      <Switch>
         <Route path="/" component={UserProfile} exact/>
         <Redirect to="/"/>
-      </>
+      </Switch>
     );
   }
 
   return (
     <CriticalErrosBoundary>
       <Router>
-        <Switch>
-          {routes}
-        </Switch>
+        {routes}
       </Router>
     </CriticalErrosBoundary>
   );
